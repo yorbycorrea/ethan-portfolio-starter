ef13050c828b720ccac18285cb876643cc4cb655
Yorby Correa Pacherres <yorbyaries.98@gmail.com>
2023-10-17 00:03:00 -0500

    Terminado las particulas de animacion

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,18 @@
+//next image
+import Image from "next/image";
+
+//next Link
+import Link from "next/link";
+
+//components
+import Socials from "../components/Socials";
+
 const Header = () => {
-  return <header>Header</header>;
+  return (
+    <header className="absolute z-30 w-full flex items-center px-16 xl:px:0 xl:h-[90px]">
+      <div className="container mx-auto">header</div>
+    </header>
+  );
 };
 
 export default Header;
--- components/ProjectsBtn.js
+++ components/ProjectsBtn.js
@@ -1,5 +1,30 @@
+// next image
+import Image from "next/image";
+
+//next link
+import Link from "next/link";
+
+//icons
+import { HiArrowRight } from "react-icons/hi2";
+
 const ProjectsBtn = () => {
-  return <div>Project Btn</div>;
+  return (
+    <div className="mx-auto xl:mx-0">
+      <Link
+        href={"/work"}
+        className="relative w-[120px] h-[120px] flex justify-center items-center bg-circleStar bg-cover bg-center bg-no-repeat group"
+      >
+        <Image
+          src={"/rounded-text.png"}
+          width={100}
+          height={107}
+          alt=""
+          className="animate-spin-slow w-full h-full max-w-[90px] max-h-[98px]"
+        />
+        <HiArrowRight className="absolute text-3xl group-hover:translate-x-2 transition-all duration-300" />
+      </Link>
+    </div>
+  );
 };
 
 export default ProjectsBtn;
